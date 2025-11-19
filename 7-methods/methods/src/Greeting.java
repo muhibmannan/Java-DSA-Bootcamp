797956bcd70a5e6b2382c04d20b22f795b0b740f
Muhib Mannan <muhibmannan@outlook.com>
2025-11-20 06:45:01 +1100

    Started learning fuction/methods. Practised creating methods, passing values, returning values, printing message.

---
+++ 7-methods/methods/src/Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+        String greeting = "Good morning";
+        return greeting;
+    }
+}
